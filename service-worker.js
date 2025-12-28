35b43f5224b137a1830f5d94e5bfdabe050bec84
BlackGWhiteee <guilhermesilva3471@gmail.com>
2025-12-28 20:12:42 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,33 @@
-const CACHE_NAME = "rotas-app-v1";
+const CACHE_NAME = "rotas-app-v2"; // MUDEI A VERSÃO
 const FILES = [
   "./",
   "./index.html",
   "./style.css",
   "./script.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
