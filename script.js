44e1d4d082171741605c37d1dc62db03030088b6
BlackGWhiteee <guilhermesilva3471@gmail.com>
2025-12-28 21:24:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ const tabRoutes = document.getElementById("tabRoutes");
 const goMap = document.getElementById("goMap");
 const goRoutes = document.getElementById("goRoutes");
 
+/* MAPA */
 const map = L.map("map").setView([-15.78, -47.93], 5);
 
 const normal = L.tileLayer(
-  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  { maxZoom: 19 }
 ).addTo(map);
 
 const satellite = L.tileLayer(
-  "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+  "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+  { maxZoom: 19 }
+);
+
+// overlay de ruas (nomes)
+const labels = L.tileLayer(
+  "https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png",
+  { maxZoom: 19 }
 );
 
 let satOn = false;
@@ -18,27 +27,29 @@ let satOn = false;
 document.getElementById("btnToggleMap").onclick = () => {
   if (satOn) {
     map.removeLayer(satellite);
+    map.removeLayer(labels);
     normal.addTo(map);
   } else {
     map.removeLayer(normal);
     satellite.addTo(map);
+    labels.addTo(map);
   }
   satOn = !satOn;
 };
 
-// LOCALIZAÇÃO
+/* LOCALIZAÇÃO AUTOMÁTICA */
 document.getElementById("btnLocate").onclick = () => {
   navigator.geolocation.getCurrentPosition(pos => {
     map.setView([pos.coords.latitude, pos.coords.longitude], 16);
   });
 };
 
-// DEFINIR LOCAL MANUAL
+/* DEFINIR LOCALIZAÇÃO MANUAL */
 document.getElementById("btnDefine").onclick = () => {
   alert("Clique no mapa para definir sua localização");
 };
 
-// TABS
+/* TABS */
 function abrir(tab) {
   tabMap.classList.remove("active");
   tabRoutes.classList.remove("active");
@@ -58,5 +69,5 @@ function abrir(tab) {
 goMap.onclick = () => abrir("map");
 goRoutes.onclick = () => abrir("routes");
 
-// GARANTIA
+/* GARANTIA FINAL */
 setTimeout(() => map.invalidateSize(), 300);
